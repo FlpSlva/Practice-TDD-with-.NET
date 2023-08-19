c559e0aaea57f9669ed2753ab31075de2dc15227
Felipe Silva <flpslvadev@gmail.com>
2023-08-19 17:39:09 -0300

    test: creating a new test of product

---
+++ tests/UnitTests/Catalog/Product/Domain/Entities/ProductTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace UnitTests.Catalog.Product.Domain.Entities;
+public class ProductTest
+{
+    [Fact(DisplayName = "")]
+    [Trait("Domain", "Product - Aggregates")]
+    public void ShouldInstantiate()
+    {
+        var exampleProduct = new
+        {
+            Name = "name",
+            Description = "description",
+
+        };
+
+        var product = new Product(exampleProduct.Name, exampleProduct.Description);
+
+        Assert.NotNull(product);
+        Assert.Equal(exampleProduct.Name, product.Name);
+        Assert.Equal(exampleProduct.Description, product.Description);
+    }
+}
