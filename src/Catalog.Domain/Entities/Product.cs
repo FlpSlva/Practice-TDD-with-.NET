7b098a7b273fd570d9b910f7b014e954769d38b4
Felipe Silva <flpslvadev@gmail.com>
2023-08-20 13:08:28 -0300

    feat: creating a new property

--- src/Catalog.Domain/Entities/Product.cs
+++ src/Catalog.Domain/Entities/Product.cs
@@ -4,6 +4,7 @@ public class Product
     public Product(string name, string description)
     {
         Id = Guid.NewGuid();
+        IsActive = true;
         Name = name;
         Description = description;
         CreatedAt = DateTime.Now;
@@ -12,4 +13,5 @@ public class Product
     public string Name { get; private set; }
     public string Description { get; private set; }
     public DateTime CreatedAt { get; private set; }
+    public bool IsActive { get; private set; }
 }
