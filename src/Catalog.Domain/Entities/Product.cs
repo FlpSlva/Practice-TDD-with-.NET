273d823c149961dbf5fe3e9062dffc145adab5dd
Felipe Silva <flpslvadev@gmail.com>
2023-08-20 14:21:43 -0300

    feat: creating new method of validation

--- src/Catalog.Domain/Entities/Product.cs
+++ src/Catalog.Domain/Entities/Product.cs
@@ -1,17 +1,28 @@
-﻿namespace Catalog.Domain.Entities;
+﻿using Catalog.Domain.Exceptions;
+
+namespace Catalog.Domain.Entities;
 public class Product
 {
-    public Product(string name, string description, bool? isActive = null)
+    public Product(string name, string description, bool isActive = true)
     {
         Id = Guid.NewGuid();
-        IsActive = isActive ?? true;
+        IsActive = isActive;
         Name = name;
         Description = description;
         CreatedAt = DateTime.Now;
+        Validate();
     }
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
     public DateTime CreatedAt { get; private set; }
-    public bool? IsActive { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+            throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+        if (Description == null)
+            throw new EntityValidationException($"{nameof(Description)} should not be null");
+    }
 }
